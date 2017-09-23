ebae22d7b46b02a0b854c5848b182a50ce4238ed
absanyal <absanyal.ABS@gmail.com>
2017-09-23 20:40:12 +0530

    Aesthetic changes made

--- Progressbar.py
+++ Progressbar.py
@@ -2,6 +2,7 @@ import time
 import sys
 import os
 import random
+import numpy as np
 
 def _print(string):
     sys.stdout.write(string)
@@ -13,10 +14,12 @@ def s_print(string, sleep_time_min = 0.1, sleep_time_max = 0.1):
         _print(i)
         time.sleep(st)
 
+char_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !.,;:"
+char_list = list( char_list )
 
-text = "Some random stuff"
+text = np.random.choice(char_list, size = 50)
 
-s_print(text)
+s_print(text, 0.0, 0.0)
 print('\n')
 
 os.system('cls')
@@ -24,25 +27,26 @@ os.system('cls')
 xm = 0
 n = 1
 x_min = 0
-x_max = 15
+x_max = 50
 x = x_min
 while ( x <= x_max):
     if ( n % 4 == 0 ):
-        w = '/'
+        w = '(  '
     if ( n % 4 == 1 ):
-        w = '-'
+        w = ' | '
     if ( n % 4 == 2 ):
-        w = '\\'
+        w = '  )'
     if ( n % 4 == 3 ):
-        w = '|'
+        w = ' | '
     xp = int( ( ( x - x_min ) / ( x_max - x_min ) * 100) )
-    xm = int( xp / 5 )
+    xm = int( xp / 10 )
     n += 1
-    b = ("Progress: " + w + " "  + "#" * xm + " " + str( xp ) +  "% ")
+    b = ("Progress: " + w + " "  + "!" * xm + " " + str( xp ) +  "% ")
     sys.stdout.write( '\r' + b )
     time.sleep(random.uniform(0.1, 1.0))
     x += random.uniform(0.1, 2.0)
 
 sys.stdout.write( '\r' + " " * len( b ) )
-b = ("Progress: " + "Done!" ) #+ " "  + "#" * ( xm + 1 ) + " " + str( 100 ) +  "% ")
+b = ("Progress: " + "Done!" )
 sys.stdout.write( '\r' + b )
+time.sleep(1.0)
